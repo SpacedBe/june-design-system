927da6f3ab472e001a37f8156629720af7f055df
Nena Penel <nena.penel@student.howest.be>
2019-03-07 16:06:41 +0100

    changing buttons to styled components

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -6,11 +6,13 @@
 import * as React from 'react';
 import {Loader} from "../loader/Loader";
 // todo: refactor to style components
-import styles from "./button.scss";
+// import styles from "./button.scss";
+import styled from 'styled-components';
+import { loadStyleVariables } from "../../scripts/loadStyleVariables";
 
 
-var classNames = require('classnames/bind');
-const cx = classNames.bind(styles);
+// var classNames = require('classnames/bind');
+// const cx = classNames.bind(styles);
 
 type Props = {
   id?: string,
@@ -35,15 +37,47 @@ type Props = {
   type?: string,
   onClick?: (id: number) => void,
   successMessage?: any[],
-  classNames?: string[],
+  // classNames?: string[],
 };
 
+const styleVariables = loadStyleVariables();
+
+const NormalButton = styled.button`
+  background-color: ${styleVariables.green};
+  border: 2px solid ${styleVariables.green};
+  border-radius: ${styleVariables.compRadius};
+  color: ${styleVariables.white};
+  cursor: pointer;
+  font-family: ${styleVariables.fontbuttons};
+  font-weight: 600;
+  line-height: (${styleVariables.compSize} - 4px); // 4px = border-top + border-bottom
+  outline: 0;
+  padding: 0 ${styleVariables.gutter};
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  letter-spacing: 0.8px;
+`
+
+const Icon = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const IconLeft = styled.span`
+  margin-right: 8px;
+`
+
+const Label = styled.span`
+  @include transition(opacity);
+    opacity: 1;
+`
 export class Button extends React.Component<Props> {
   render() {
     let buttonContent;
     let isDisabled = (this.props.loading || this.props.disabled);
 
-    let Tag = 'button';
+    let Tag = 'div';
 
     if (this.props.href) {
       Tag = 'a';
@@ -57,69 +91,84 @@ export class Button extends React.Component<Props> {
 
     if (hasIcon && this.props.iconLeft) {
       buttonContent = (
-        <div>
-          <div className={styles.icon}>
-            <span className={styles["icon-left"]}>{this.props.iconLeft}</span>
-            <span className={styles.label}>{this.props.children}</span>
-          </div>
+        // <NormalButton>
+        //   <div className={styles.icon}>
+        //     <span className={styles["icon-left"]}>{this.props.iconLeft}</span>
+        //     <span className={styles.label}>{this.props.children}</span>
+        //   </div>
+        //   <Loader loading={this.props.loading} percentage={this.props.percentageDone}/>
+        // </NormalButton>
+        <NormalButton>
+          <Icon>
+            <IconLeft>{this.props.iconLeft}</IconLeft>
+            <Label>{this.props.children}</Label>
+          </Icon>
           <Loader loading={this.props.loading} percentage={this.props.percentageDone}/>
-        </div>
+        </NormalButton>
       )
 
     }
 
     if (hasIcon && this.props.iconRight) {
       buttonContent = (
-        <div>
-          <div className={styles.icon}>
-            <span className={styles.label}>{this.props.children}</span>
-            <span className={styles["icon-right"]}>{this.props.iconRight}</span>
+        <NormalButton>
+          <div>
+            <span>{this.props.children}</span>
+            <span>{this.props.iconRight}</span>
           </div>
           <Loader loading={this.props.loading} percentage={this.props.percentageDone}/>
-        </div>
+        </NormalButton>
       )
     }
 
     if (hasIcon && this.props.iconOnly) {
       buttonContent = (
-        <div>
-          <div className={styles.icon}>
+        <NormalButton>
+          <div>
             {this.props.iconOnly}
           </div>
-        </div>
+        </NormalButton>
       )
     }
 
     if (!hasIcon) {
       buttonContent = (
+        // <NormalButton>
+        //   <span className={styles.label}>{this.props.children}</span>
+        //   <Loader loading={this.props.loading} percentage={this.props.percentageDone}/>
+        // </NormalButton>
         <div>
-          <span className={styles.label}>{this.props.children}</span>
-          <Loader loading={this.props.loading} percentage={this.props.percentageDone}/>
+          <NormalButton>
+            <span>{this.props.children}</span>
+            <Loader loading={this.props.loading} percentage={this.props.percentageDone}/>
+          </NormalButton>
         </div>
+
+        
       )
     }
 
 
-    const className = cx(
-      'button',
-      {[`button--${this.props.color}`]: !!this.props.color},
-      {'button--wide': this.props.wide},
-      {'button--spaced': this.props.spaced},
-      {'button--clear': this.props.clear},
-      {'button--small': this.props.size === 'small'},
-      {'button--large': this.props.size === 'large'},
-      {'button--loading': this.props.loading},
-      {'button--outlined': this.props.outlined},
-      {'button--round': this.props.rounded},
-      {'button--icon': hasIcon},
-      this.props.classNames,
-    );
+    // const className = cx(
+    //   'button',
+    //   {[`button--${this.props.color}`]: !!this.props.color},
+    //   {'button--wide': this.props.wide},
+    //   {'button--spaced': this.props.spaced},
+    //   {'button--clear': this.props.clear},
+    //   {'button--small': this.props.size === 'small'},
+    //   {'button--large': this.props.size === 'large'},
+    //   {'button--loading': this.props.loading},
+    //   {'button--outlined': this.props.outlined},
+    //   {'button--round': this.props.rounded},
+    //   {'button--icon': hasIcon},
+    //   this.props.classNames,
+    // );
 
     return (
       //@ts-ignore
       <Tag
         id={this.props.id}
-        className={className}
+        // className={className}
         onClick={this.props.onClick}
         disabled={isDisabled}
         href={this.props.href}
