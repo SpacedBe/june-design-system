5aa226b52798d2608d49cc66e47ad543276b1862
Nena Penel <nena.penel@student.howest.be>
2019-03-14 14:22:39 +0100

    setted all the form items

--- example/src/pages/form/formikForm.jsx
+++ example/src/pages/form/formikForm.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Page, render } from "catalog";
-import { FormikInput, FormikSelect } from "june-design-system";
+import { FormikInput, FormikSelect, FormikRadiobutton, FormikToggle, IconQuestionmark, IconElectricity, Button } from "june-design-system";
 import styled from "styled-components";
-import Autocomplete from "../components/autocomplete";
 
 export default class FormPage extends React.Component{
   constructor(props) {
@@ -20,7 +19,6 @@ export default class FormPage extends React.Component{
       }
     };
   }
-
 
   render(){
     return (
@@ -73,10 +71,12 @@ export default class FormPage extends React.Component{
           </div>
           <FormikSelect
             label="Gender"
-            options={[{ option: "ONE" },
-            { option: "TWO" },
-            { option: "THREE" },
-            { option: "FOUR" }]}
+            options={[
+              { option: "ONE" },
+              { option: "TWO" },
+              { option: "THREE" },
+              { option: "FOUR" }
+            ]}
             htmlFor="isSelect"
             error={this.state.error}
             touched={this.state.touched}
@@ -84,6 +84,78 @@ export default class FormPage extends React.Component{
             field={this.state.field}
             form={this.state.form}
           />
+
+          <div>
+            <p>The question linked to the radio buttons is asked here.*</p>
+            <FormikRadiobutton
+              error={this.state.error}
+              label={this.state.label}
+              type="radio"
+              htmlFor="isRadioButton"
+              name="radioCheck"
+              label="option one"
+              checked={this.state.checked}
+            />
+            <FormikRadiobutton
+              error={this.state.error}
+              label={this.state.label}
+              type="radio"
+              htmlFor="isRadioButton"
+              name="radioCheck"
+              label="When a label is really long it just shows on multiple lines."
+              checked={this.state.checked}
+            />
+          </div>
+
+          <div>
+            <p>The question linked to the checkboxes is asked here.</p>
+          </div>
+        </div>
+        <h2>Form Section Title</h2>
+        <p>Select atleast one option below.*</p>
+
+          <FormikToggle
+            tooltipToggle={<IconQuestionmark />}
+            iconRight={<IconElectricity />}
+            label="Toggle with a label"
+            error={this.state.error}
+            touched={this.state.touched}
+            disabled={this.state.disabled}
+            field={this.state.field}
+            form={this.state.form}
+          />
+
+          <FormikToggle
+            tooltipToggle={<IconQuestionmark />}
+            label="Regular Toggle"
+            error={this.state.error}
+            touched={this.state.touched}
+            disabled={this.state.disabled}
+            field={this.state.field}
+            form={this.state.form}
+          />
+
+          <FormikToggle
+            label="Toggle with a long label discription"
+            error={this.state.error}
+            touched={this.state.touched}
+            disabled={this.state.disabled}
+            field={this.state.field}
+            form={this.state.form}
+          />
+
+          <FormikToggle
+            iconRight={<IconElectricity />}
+            label="Toggle with a label"
+            error={this.state.error}
+            touched={this.state.touched}
+            disabled={this.state.disabled}
+            field={this.state.field}
+            form={this.state.form}
+          />
+        <div />
+        <div>
+          <Button disabled={this.state.disabled}>verzenden</Button>
         </div>
       </Page>
     );
