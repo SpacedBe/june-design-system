a121d03cdfba1bea14e03c858c865cdec2b1d6b8
PhilippeSchraepen <mail@phill.be>
2019-04-04 13:18:50 +0200

    Remove margins from inputcomponents

--- src/components/formik-textarea/FormikTextarea.tsx
+++ src/components/formik-textarea/FormikTextarea.tsx
@@ -4,7 +4,7 @@
 
 import * as React from 'react';
 import styled from 'styled-components';
-import { loadStyleVariables } from "../../scripts/loadStyleVariables";
+import {loadStyleVariables} from "../../scripts/loadStyleVariables";
 
 const sizes = {
   small: "50px",
@@ -50,7 +50,7 @@ const InputBoxRight = styled.div<{ error?: boolean; disabled?: boolean, focussed
   display: flex;
   justify-content: space-between;
   height: ${props => sizes[props.size || "medium"]};
-  margin-bottom: 20px;
+
   &::placeholder {
      color: ${props => props.error ? `${styleVariables.colorRed}` : `${styleVariables.colorGrayLight}`};
   }
@@ -63,20 +63,20 @@ const Textarea = styled.textarea<{ error?: boolean; disabled?: boolean }>`
   opacity: ${props => props.disabled ? '0.5' : '1'};
   width: 100%;
 
-`
+`;
 
 const Flex = styled.div`
   display: flex;
   align-items: center;
   width: 100%;
   justify-content: space-between;
-`
+`;
 
 const Label = styled.label<{ disabled?: boolean; error?: boolean }>`
   padding-bottom: 5px;
   opacity: ${props => (props.disabled ? "0.5" : "1")};
   color: ${props =>
-    props.error ? `${styleVariables.colorRed}` : `${styleVariables.black}`};
+  props.error ? `${styleVariables.colorRed}` : `${styleVariables.black}`};
   font-family: ${styleVariables.fontSecondary};
   font-size: ${styleVariables.fontSizeM};
 `;
@@ -85,25 +85,28 @@ export class FormikTextarea extends React.Component<Props> {
   static defaultProps = {
     size: 'medium',
   };
+
   render() {
     const FieldName = this.props.field.name;
     const errors = (this.props.serverErrors && this.props.serverErrors[FieldName]) || this.props.form.errors[FieldName];
     const touched = this.props.form.touched[FieldName];
     let buttonContent;
-      buttonContent = (
-        <div>
-          <Flex>
-            <Label error={this.props.error} disabled={this.props.disabled} htmlFor={FieldName}>
-              {this.props.label}
-            </Label>
-          </Flex>
-          <InputBoxRight size={this.props.size} error={this.props.error} disabled={this.props.disabled} focussed={this.props.focussed}>
-            <Textarea {...this.props.field} placeholder={this.props.placeholderText} disabled={this.props.disabled}></Textarea>
-          </InputBoxRight>
-        </div>
-      )
+    buttonContent = (
+      <div>
+        <Flex>
+          <Label error={this.props.error} disabled={this.props.disabled} htmlFor={FieldName}>
+            {this.props.label}
+          </Label>
+        </Flex>
+        <InputBoxRight size={this.props.size} error={this.props.error} disabled={this.props.disabled}
+                       focussed={this.props.focussed}>
+          <Textarea {...this.props.field} placeholder={this.props.placeholderText}
+                    disabled={this.props.disabled}></Textarea>
+        </InputBoxRight>
+      </div>
+    );
     return (
-      <span >
+      <span>
         {buttonContent}
         <span>
           {
@@ -111,6 +114,6 @@ export class FormikTextarea extends React.Component<Props> {
           }
         </span>
       </span>
-    )
+    );
   }
 }
