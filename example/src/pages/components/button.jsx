53d67fd255284ecc31fb32bc118d23dede7285e1
Nena Penel <nena.penel@student.howest.be>
2019-04-24 11:51:16 +0200

    changes prq + added new scss file

--- example/src/pages/components/button.jsx
+++ example/src/pages/components/button.jsx
@@ -60,121 +60,126 @@ export default class ButtonPage extends React.Component {
 
   render() {
     return (
-      <Page>
-        ## Buttons
-        <div className="flexBox">
+      <div className='buttons'>
+        <Page>
+          ## Buttons
+          <div className='flexBox'>
+            <ReactSpecimen>
+              <Button
+                size={this.state.size}
+                rounded={this.state.rounded}
+                clear={this.state.clear}
+                disabled={this.state.disabled}
+                wide={this.state.wide}
+                color={this.state.color}
+                onClick={this.state.clicked}
+              >
+                {this.state.content}
+              </Button>
+            </ReactSpecimen>
+            <Flex justify={'space-between'}>
+              <div className='wrapper'>
+                <label htmlFor='isRounded'>Rounded</label>
+                <input
+                  className=''
+                  type='checkbox'
+                  value={this.state.rounded}
+                  name='isRounded'
+                  onChange={() => this.changeRounded()}
+                />
+              </div>
+
+              <div className='wrapper'>
+                <label htmlFor='isDisabled'>Disabled</label>
+                <input
+                  type='checkbox'
+                  value={this.state.disabled}
+                  name='isDisabled'
+                  onChange={() => this.changeDisable()}
+                />
+              </div>
+
+              <div className='wrapper'>
+                <label htmlFor='isClear'>Clear</label>
+                <input
+                  type='checkbox'
+                  value={this.state.clear}
+                  name='isClear'
+                  onChange={() => this.changeClear()}
+                />
+              </div>
+
+              <div className='wrapper'>
+                <label htmlFor='isFullWidth'>Full width</label>
+                <input
+                  type='checkbox'
+                  value={this.state.wide}
+                  name='isFullWidth'
+                  onChange={() => this.changeWide()}
+                />
+              </div>
+
+              <div className='wrapper'>
+                <label htmlFor='color'>Color</label>
+                <select
+                  name='color'
+                  value={this.state.color}
+                  onChange={event => this.changeColor(event)}
+                >
+                  <option value='primary'>green</option>
+                  <option value='error'>red</option>
+                  <option value='facebook'>blue</option>
+                </select>
+              </div>
+
+              <div className='wrapper'>
+                <label htmlFor='size'>Size</label>
+                <select
+                  name='size'
+                  value={this.state.size}
+                  onChange={event => this.changeSize(event)}
+                >
+                  <option value='small'>Small</option>
+                  <option value='medium'>Medium</option>
+                  <option value='large'>Large</option>
+                </select>
+              </div>
+            </Flex>
+          </div>
+          ## Icon on the left
           <ReactSpecimen>
-            <Button
-              size={this.state.size}
-              rounded={this.state.rounded}
-              clear={this.state.clear}
-              disabled={this.state.disabled}
-              wide={this.state.wide}
-              color={this.state.color}
-              onClick={this.state.clicked}
-            >
-              {this.state.content}
+            <Button iconLeft={<IconSettings />}>Button with icon</Button>
+          </ReactSpecimen>
+          ## Icon on the right
+          <ReactSpecimen>
+            <Button iconRight={<IconSettings />}> Button with icon</Button>
+          </ReactSpecimen>
+          ## Only an icon
+          <ReactSpecimen>
+            <Button iconOnly={<IconSettings />}>
+              Button with only an icon
             </Button>
           </ReactSpecimen>
-          <Flex justify={'space-between'}>
-            <div className='div'>
-              <label htmlFor='isRounded'>Rounded</label>
-              <input
-                className=''
-                type='checkbox'
-                value={this.state.rounded}
-                name='isRounded'
-                onChange={() => this.changeRounded()}
-              />
-            </div>
-
-            <div className='div'>
-              <label htmlFor='isDisabled'>Disabled</label>
-              <input
-                type='checkbox'
-                value={this.state.disabled}
-                name='isDisabled'
-                onChange={() => this.changeDisable()}
-              />
-            </div>
-
-            <div className='div'>
-              <label htmlFor='isClear'>Clear</label>
-              <input
-                type='checkbox'
-                value={this.state.clear}
-                name='isClear'
-                onChange={() => this.changeClear()}
-              />
-            </div>
-
-            <div className='div'>
-              <label htmlFor='isFullWidth'>Full width</label>
-              <input
-                type='checkbox'
-                value={this.state.wide}
-                name='isFullWidth'
-                onChange={() => this.changeWide()}
-              />
-            </div>
-
-            <div className='div'>
-              <label htmlFor='color'>Color</label>
-              <select
-                name='color'
-                value={this.state.color}
-                onChange={event => this.changeColor(event)}
-              >
-                <option value='primary'>green</option>
-                <option value='error'>red</option>
-                <option value='facebook'>blue</option>
-              </select>
-            </div>
-
-            <div className='div'>
-              <label htmlFor='size'>Size</label>
-              <select
-                name='size'
-                value={this.state.size}
-                onChange={event => this.changeSize(event)}
-              >
-                <option value='small'>Small</option>
-                <option value='medium'>Medium</option>
-                <option value='large'>Large</option>
-              </select>
-            </div>
-          </Flex>
-        </div>
-        ## Icon on the left
-        <ReactSpecimen>
-          <Button iconLeft={<IconSettings />}>Button with icon</Button>
-        </ReactSpecimen>
-        ## Icon on the right
-        <ReactSpecimen>
-          <Button iconRight={<IconSettings />}> Button with icon</Button>
-        </ReactSpecimen>
-        ## Only an icon
-        <ReactSpecimen>
-          <Button iconOnly={<IconSettings />}>
-            Button with only an icon
-          </Button>
-        </ReactSpecimen>
-        ## Only an icon, with border
-        <ReactSpecimen>
-          <Button iconButtonWithBorder={<IconSettings />}/>
-        </ReactSpecimen>
-        ## Loading
-        <ReactSpecimen>
-          <Button loading='true'>Loading button</Button>
-        </ReactSpecimen>
-        # Click events
-        <ReactSpecimen>
-          <Button htmlfor='isClicked' onClick={() => this.changeClicked()}>
-            Click
-          </Button>
-        </ReactSpecimen>
-        <p>Clicked {this.state.clicked} times</p>
-      </Page>
+          ## Only an icon, with border
+          <ReactSpecimen>
+            <Button iconButtonWithBorder={<IconSettings />} />
+          </ReactSpecimen>
+          ## Loading
+          <ReactSpecimen>
+            <Button loading='true'>Loading button</Button>
+          </ReactSpecimen>
+          # Click events
+          <ReactSpecimen>
+            <Button
+              htmlfor='isClicked'
+              onClick={() => this.changeClicked()}
+            >
+              Click
+            </Button>
+          </ReactSpecimen>
+          <p>Clicked {this.state.clicked} times</p>
+        </Page>
+      </div>
     );
   }
 }
