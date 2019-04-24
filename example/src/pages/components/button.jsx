1b26722da3b3ea087e336981d4da9f50bf933b15
Nena Penel <nena.penel@student.howest.be>
2019-04-24 09:35:49 +0200

    changed layout control buttons Button Comp.

--- example/src/pages/components/button.jsx
+++ example/src/pages/components/button.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Page, ReactSpecimen} from 'catalog';
-import {Button, IconSettings} from 'june-design-system';
+import {Button, IconSettings, FormikCheckbox} from 'june-design-system';
 
 export default class ButtonPage extends React.Component {
   constructor(props) {
@@ -61,84 +61,87 @@ export default class ButtonPage extends React.Component {
     return (
       <Page>
         ## Buttons
-        <ReactSpecimen>
-          <Button
-            size={this.state.size}
-            rounded={this.state.rounded}
-            clear={this.state.clear}
-            disabled={this.state.disabled}
-            wide={this.state.wide}
-            color={this.state.color}
-            onClick={this.state.clicked}
-          >
-            {this.state.content}
-          </Button>
-        </ReactSpecimen>
-        <div>
-          <div>
-            <label htmlFor='isRounded'>Rounded?</label>
-            <input
-              type='checkbox'
-              value={this.state.rounded}
-              name='isRounded'
-              onChange={() => this.changeRounded()}
-            />
-          </div>
-
-          <div>
-            <label htmlFor='isDisabled'>Disabled?</label>
-            <input
-              type='checkbox'
-              value={this.state.disabled}
-              name='isDisabled'
-              onChange={() => this.changeDisable()}
-            />
-          </div>
-
-          <div>
-            <label htmlFor='isClear'>Clear?</label>
-            <input
-              type='checkbox'
-              value={this.state.clear}
-              name='isClear'
-              onChange={() => this.changeClear()}
-            />
-          </div>
-
-          <div>
-            <label htmlFor='isFullWidth'>Full width?</label>
-            <input
-              type='checkbox'
-              value={this.state.wide}
-              name='isFullWidth'
-              onChange={() => this.changeWide()}
-            />
-          </div>
-
-          <div>
-            <label htmlFor='color'>Color</label>
-            <select
-              name='color'
-              value={this.state.color}
-              onChange={event => this.changeColor(event)}
+        <div className="flexBox">
+          <ReactSpecimen>
+            <Button
+              size={this.state.size}
+              rounded={this.state.rounded}
+              clear={this.state.clear}
+              disabled={this.state.disabled}
+              wide={this.state.wide}
+              color={this.state.color}
+              onClick={this.state.clicked}
             >
-              <option value='primary'>green</option>
-              <option value='error'>red</option>
-              <option value='facebook'>blue</option>
-            </select>
-          </div>
+              {this.state.content}
+            </Button>
+          </ReactSpecimen>
+          <div className="flex">
+            <div className='div'>
+              <label htmlFor='isRounded'>Rounded</label>
+              <input
+                className=''
+                type='checkbox'
+                value={this.state.rounded}
+                name='isRounded'
+                onChange={() => this.changeRounded()}
+              />
+            </div>
 
-          <div>
-            <label htmlFor='size'>Size</label>
-            <select
-              name='size'
-              value={this.state.size}
-              onChange={event => this.changeSize(event)}
-            >
-              <option value='small'>Small</option>
-              <option value='medium'>Medium</option>
-              <option value='large'>Large</option>
-            </select>
+            <div className='div'>
+              <label htmlFor='isDisabled'>Disabled</label>
+              <input
+                type='checkbox'
+                value={this.state.disabled}
+                name='isDisabled'
+                onChange={() => this.changeDisable()}
+              />
+            </div>
+
+            <div className='div'>
+              <label htmlFor='isClear'>Clear</label>
+              <input
+                type='checkbox'
+                value={this.state.clear}
+                name='isClear'
+                onChange={() => this.changeClear()}
+              />
+            </div>
+
+            <div className='div'>
+              <label htmlFor='isFullWidth'>Full width</label>
+              <input
+                type='checkbox'
+                value={this.state.wide}
+                name='isFullWidth'
+                onChange={() => this.changeWide()}
+              />
+            </div>
+
+            <div className='div'>
+              <label htmlFor='color'>Color</label>
+              <select
+                name='color'
+                value={this.state.color}
+                onChange={event => this.changeColor(event)}
+              >
+                <option value='primary'>green</option>
+                <option value='error'>red</option>
+                <option value='facebook'>blue</option>
+              </select>
+            </div>
+
+            <div className='div'>
+              <label htmlFor='size'>Size</label>
+              <select
+                name='size'
+                value={this.state.size}
+                onChange={event => this.changeSize(event)}
+              >
+                <option value='small'>Small</option>
+                <option value='medium'>Medium</option>
+                <option value='large'>Large</option>
+              </select>
+            </div>
           </div>
         </div>
         ## Icon on the left
