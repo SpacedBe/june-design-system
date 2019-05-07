a40f64cd5521de9e68735108ee24f36625189bfe
Nena Penel <nena.penel@student.howest.be>
2019-05-07 13:02:18 +0200

    changed top checkboxes toggle

--- example/src/pages/form/formikToggle.jsx
+++ example/src/pages/form/formikToggle.jsx
@@ -34,61 +34,49 @@ export default class FormikTogglePage extends React.Component {
     });
   }
 
-
   render() {
     return (
       <Page>
-        <Flex>
-          <div className='wrapper'>
-            <FormikCheckbox
-              error={false}
-              field={{
-                name: 'isDisabled',
-                value: this.state.disabled,
-                onChange: () => this.toggleDisabled()
-              }}
-              form={{
-                errors: { 'regular': null },
-                touched: { 'regular': false }
-              }}
-              label='Disabled'
-              type='checkbox'
-            />
-          </div>
-          <div className='wrapper'>
-            <FormikCheckbox
-              error={false}
-              field={{
-                name: 'isTouched',
-                value: this.state.focussed,
-                onChange: () => this.toggleTouched()
-              }}
-              form={{
-                errors: {'example-input': null},
-                touched: {'example-input': false}
-              }}
-              label='Touched'
-              type='checkbox'
-            />
-          </div>
+        <Formik>
+          <Form>
+            <Flex>
+              <FormGroup className='wrapper'>
+                <Field
+                  error={false}
+                  field={{
+                    name: 'isTouched',
+                    value: this.state.focussed,
+                    onChange: () => this.toggleTouched()
+                  }}
+                  form={{
+                    errors: { 'example-input': null },
+                    touched: { 'example-input': false }
+                  }}
+                  label='Touched'
+                  type='checkbox'
+                  component={FormikCheckbox}
+                />
+              </FormGroup>
+              <FormGroup className='wrapper'>
+                <Field
+                  error={false}
+                  field={{
+                    name: 'isDisabled',
+                    onChange: () => this.toggleDisabled()
+                  }}
+                  form={{
+                    errors: { 'example-input': null },
+                    touched: { 'example-input': false }
+                  }}
+                  label='Disabled'
+                  type='checkbox'
+                  component={FormikCheckbox}
+                />
+              </FormGroup>
+            </Flex>
+          </Form>
+        </Formik>
 
-          <div className='wrapper'>
-            <FormikCheckbox
-              error={false}
-              field={{
-                name: 'hasError',
-                value: this.state.error,
-                onChange: () => this.toggleError()
-              }}
-              form={{
-                errors: {'example-input': null},
-                touched: {'example-input': false}
-              }}
-              label='Error'
-              type='checkbox'
-            />
-          </div>
-        </Flex>
          ## Toggle Regular
         <ReactSpecimen span={3}>
           <Formik>
