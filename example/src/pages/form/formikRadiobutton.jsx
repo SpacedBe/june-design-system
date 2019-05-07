309972317f52089b5dac4b3fe0899a57ce65b4b8
nenapenel <33931603+nenapenel@users.noreply.github.com>
2019-05-07 14:23:30 +0200

    Radiobutton to formik (#113)
    * radiobutton to formikRadiobutton
    
    * changed top checkboxes

--- example/src/pages/form/formikRadiobutton.jsx
+++ example/src/pages/form/formikRadiobutton.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import {Page, ReactSpecimen} from "catalog";
-import {FormGroup, FormikCheckbox, FormikRadiobutton, IconQuestionmark} from "june-design-system";
+import {FormGroup, FormikCheckbox, FormikRadiobutton, IconQuestionmark, Button} from "june-design-system";
 import {Flex} from 'reflexbox';
+import { Field, Form, Formik } from 'formik';
 
 export default class FormikRadiobuttonPage extends React.Component{
   constructor(props){
@@ -69,113 +70,122 @@ export default class FormikRadiobuttonPage extends React.Component{
 
       <Page>
           ## Radiobutton
-          <Flex>
-            <div className='wrapper'>
-              <FormikCheckbox
-                error={false}
-                field={{
-                  name: 'isTouched',
-                  value: this.state.focussed,
-                  onChange: () => this.toggleTouched()
-                }}
-                form={{
-                  errors: { 'example-input': null },
-                  touched: { 'example-input': false }
-                }}
-                label='Touched'
-                type='checkbox'
-              />
-            </div>
-
-            <div className='wrapper'>
-              <FormikCheckbox
-                error={false}
-                field={{
-                  name: 'hasError',
-                  value: this.state.error,
-                  onChange: () => this.toggleError()
-                }}
-                form={{
-                  errors: { 'example-input': null },
-                  touched: { 'example-input': false }
-                }}
-                label='Error'
-                type='checkbox'
-              />
-            </div>
-
-            <div className='wrapper'>
-              <FormikCheckbox
-                error={false}
-                field={{
-                  name: 'hasServerError',
-                  onChange: () => this.toggleServerError()
-                }}
-                form={{
-                  errors: { 'example-input': null },
-                  touched: { 'example-input': false }
-                }}
-                label='Has Server Error'
-                type='checkbox'
-              />
-            </div>
-
-            <div className='wrapper'>
-              <FormikCheckbox
-                error={false}
-                field={{
-                  name: 'isDisabled',
-                  onChange: () => this.toggleDisabled()
-                }}
-                form={{
-                  errors: { 'example-input': null },
-                  touched: { 'example-input': false }
-                }}
-                label='Disabled'
-                type='checkbox'
-              />
-            </div>
-          </Flex>
+        <Formik>
+          <Form>
+            <Flex>
+              <FormGroup className='wrapper'>
+                <Field
+                  error={false}
+                  field={{
+                    name: 'isTouched',
+                    value: this.state.focussed,
+                    onChange: () => this.toggleTouched()
+                  }}
+                  form={{
+                    errors: { 'example-input': null },
+                    touched: { 'example-input': false }
+                  }}
+                  label='Touched'
+                  type='checkbox'
+                  component={FormikCheckbox}
+                />
+              </FormGroup>
+              <FormGroup className='wrapper'>
+                <Field
+                  error={false}
+                  field={{
+                    name: 'hasError',
+                    value: this.state.error,
+                    onChange: () => this.toggleError()
+                  }}
+                  form={{
+                    errors: { 'example-input': null },
+                    touched: { 'example-input': false }
+                  }}
+                  label='Error'
+                  type='checkbox'
+                  component={FormikCheckbox}
+                />
+              </FormGroup>
+              <FormGroup className='wrapper'>
+                <Field
+                  error={false}
+                  field={{
+                    name: 'isDisabled',
+                    onChange: () => this.toggleDisabled()
+                  }}
+                  form={{
+                    errors: { 'example-input': null },
+                    touched: { 'example-input': false }
+                  }}
+                  label='Disabled'
+                  type='checkbox'
+                  component={FormikCheckbox}
+                />
+              </FormGroup>
+              <FormGroup className='wrapper'>
+                <Field
+                  error={false}
+                  field={{
+                    name: 'hasServerError',
+                    onChange: () => this.toggleServerError()
+                  }}
+                  form={{
+                    errors: { 'example-input': null },
+                    touched: { 'example-input': false }
+                  }}
+                  label='Has Server Error'
+                  type='checkbox'
+                  component={FormikCheckbox}
+                />
+              </FormGroup>
+            </Flex>
+          </Form>
+        </Formik>
           <ReactSpecimen span={3}>
-            <div>
-              <FormGroup>
-                <FormikRadiobutton
-                  error={this.state.error}
-                  focussed={this.state.focussed}
-                  type='radio'
-                  placeholderText='example placeholder'
-                  field={this.state.field}
-                  label='Option one'
-                  form={this.state.form}
-                  name='radioTestName'
-                  disabled={this.state.disabled}
-                />
-                <FormikRadiobutton
-                  error={this.state.error}
-                  focussed={this.state.focussed}
-                  type='radio'
-                  placeholderText='example placeholder'
-                  field={this.state.field}
-                  label='When a label is really long it just shows on multiple lines.'
-                  form={this.state.form}
-                  name='radioTestName'
-                  disabled={this.state.disabled}
-                />
-              </FormGroup>
-              <FormGroup>
-                <FormikRadiobutton
-                  error={this.state.error}
-                  focussed={this.state.focussed}
-                  type='radio'
-                  placeholderText='example placeholder'
-                  field={this.state.field}
-                  label='When a label is really long it just shows on multiple lines.'
-                  form={this.state.form}
-                  name='radioTestName'
-                  tooltip={<IconQuestionmark />}
-                />
-              </FormGroup>
-            </div>
+            <Formik>
+              <Form>
+                <FormGroup>
+                  <Field
+                    error={this.state.error}
+                    focussed={this.state.focussed}
+                    type='radio'
+                    placeholderText='example placeholder'
+                    field={this.state.field}
+                    label='Option one'
+                    form={this.state.form}
+                    name='radioTestName'
+                    disabled={this.state.disabled}
+                    component={FormikRadiobutton}
+                  />
+                  <Field
+                    error={this.state.error}
+                    focussed={this.state.focussed}
+                    disabled={this.state.disabled}
+                    type='radio'
+                    placeholderText='example placeholder'
+                    field={this.state.field}
+                    label='When a label is really long it just shows on multiple lines.'
+                    form={this.state.form}
+                    name='radioTestName'
+                    component={FormikRadiobutton}
+                  />
+                  <Field
+                    error={this.state.error}
+                    focussed={this.state.focussed}
+                    disabled={this.state.disabled}
+                    type='radio'
+                    placeholderText='example placeholder'
+                    field={this.state.field}
+                    label='When a label is really long it just shows on multiple lines.'
+                    form={this.state.form}
+                    name='radioTestName'
+                    tooltip={<Button type='button' color='gray-dark' iconOnly={<IconQuestionmark />}/>}
+                    component={FormikRadiobutton}
+                  />
+                </FormGroup>
+              </Form>
+            </Formik>
           </ReactSpecimen>
         </Page>
     );
